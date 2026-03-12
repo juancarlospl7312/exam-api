ad7a683b4857b788b6f10a1d2589bba7bb1fcd94
Juan Carlos Prado López <juancarlospl7312@gmail.com>
2026-03-11 23:22:56 -0400

    edit: SubscribeRequest.php to handle failed validations

--- app/Http/Requests/SubscribeRequest.php
+++ app/Http/Requests/SubscribeRequest.php
@@ -3,7 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 
 class SubscribeRequest extends FormRequest
 {
@@ -26,5 +29,13 @@ class SubscribeRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:subscribers,email'
         ];
+    }
+
+    protected function failedValidation(Validator $validator): JsonResponse
+    {
+        $response = response()->json([
+            'message' => $validator->errors()->all(),
+        ], 422);
+        throw new ValidationException($validator, $response);
     }
 }
